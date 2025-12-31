52f3190b4962c4ca9bb709698b24f9863a1ea2c2
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 23:24:46 +0000

    Version 0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/structured-logging "0.2.1"
+(defproject org.openvoxproject/structured-logging "0.2.2-SNAPSHOT"
   :description "Write data structures to your logs from clojure."
   :url "https://github.com/openvoxproject/structured-logging"
   :license {:name "Apache License, Version 2.0"
